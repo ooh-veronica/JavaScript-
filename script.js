8a7ecf71757f303220685e593f6368f536326148
ooh-veronica <kr.veronique@gmail.com>
2021-11-10 20:56:16 +0300

    hw_1_variables

---
+++ script.js
@@ -0,0 +1,39 @@
+// ЗАДАНИЕ 1 
+
+// 1)	Объявить две переменные с любыми значениями и затем поменять значения переменных местами. Вывести в консоль значения переменных до и после.
+
+// let biography = {
+//     woman: "Veronica",
+//     favotireColor : "white",
+//     favoriteCar : "mazda",
+//     favoriteName : {
+//         names : "Ariana"
+//     },
+
+
+// };
+
+// let autobiography = {
+//     man : "Ar", 
+//     work : "Manager", 
+//     study : "BSEU", 
+//     favoriteSubject : {
+//         subject : "math"
+//     },
+// };
+
+// console.log(biography, autobiography);
+
+// [biography, autobiography] = [autobiography, biography];
+
+// console.log(biography, autobiography);
+
+
+// ЗАДАНИЕ 2 
+// 2)	Объявить массив состоящий из 10 любых чисел, сложить три любых числа из этого массива с помощью оператора + и вывести результат в консоль.
+
+// let arr = [1,2,3,4,5,6,7,8,9,100]; 
+
+// console.log(arr[0] + arr[1] + arr[9])
+
+
